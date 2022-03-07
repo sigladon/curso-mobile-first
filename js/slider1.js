d1a0a20f9c7b75ab7af441181dfec6b6227276ef
sigladon <sigladon.rc@gmail.com>
2022-03-07 11:54:55 -0500

    agregada funcionalidad al slider

--- js/slider1.js
+++ js/slider1.js
@@ -2,65 +2,40 @@ const slider1 = document.querySelector("#slider1");
 const slider2 = document.querySelector("#slider2");
 let slider1Section = document.querySelectorAll(".table-div");
 let slider2Section = document.querySelectorAll(".price");
-var slider1Counter = 0;
+slider1.value = 0;
+slider2.value = 0;
 const btnOff = "btnOff";
+const slider1Btns = document.querySelectorAll(".slider1btn");
 
-const btn1Right = document.querySelector("#slider1NextBtn");
-const btn1Left = document.querySelector("#slider1PreviousBtn");
-
-function btnChecker(counter,last,lBtn,rBtn) {
-    switch (counter) {
+function sliderFn(slider,sliderElements,sliderButtons,direction) {
+    const sliderLen = sliderElements.length;
+    switch (slider.value) {
         case 0:
-            lBtn.classList.toggle(btnOff);
-            rBtn.classList.remove(btnOff);
+            sliderButtons[0].classList.toggle(btnOff);
+            sliderButtons[1].classList.remove(btnOff);
             break;
-        case last:
-            lBtn.classList.remove(btnOff);
-            rBtn.classList.toggle(btnOff);
+        case sliderLen - 1:
+            sliderButtons[1].classList.toggle(btnOff);
+            sliderButtons[0].classList.remove(btnOff);
             break;
-
         default:
-            lBtn.classList.remove(btnOff);
-            rBtn.classList.remove(btnOff);
+            sliderButtons[0].classList.remove(btnOff);
+            sliderButtons[1].classList.remove(btnOff);
             break;
+    }
+    if (slider.value >= 0 && slider.value <= sliderLen -1) {
+        slider.value = slider.value + direction;
+        const margin = slider.value * 100;
+        slider.style.marginLeft = `-${margin}%`;
     }
 }
 
-function btnFn(dir,slide,sliderCtn,btn) {
-    const sliderLen =  sliderCtn.length;
-    switch (dir) {
-        case "left":
-            if (counter > 0) {
-                counter--;
-                const margin = counter * 100
-                slide.style.marginLeft = `-${margin}%`;
-                btn.classList.remove(btnOff);
-            } else {
-                btn.classList.toggle(btnOff);
-            }
-            break;
-            
-        case "right":
-            if (counter < sliderLen) {
-                counter++;
-                const margin = counter * 100
-                slide.style.marginLeft = `-${margin}%`;
-                btn.classList.toffle(btnOff);
-            } else {
-                btn.classList.toggle(btnOff);
-            }
-            break;
-
-        default:
-            break;
-    }
-}
-
-
-btn1Right.addEventListener('click', function(){
-    btnFn("right",slider1,slider1Counter,btn1Right);
+slider1Btns[1].addEventListener('click', function(){
+    const right = -1;
+    sliderFn(slider1,slider1Section,slider1Btns,right);
 });
 
-btn1Left.addEventListener('click', function(){
-    btnFn("left",slider1,slider1Counter,btn1Left);
+slider1Btns[0].addEventListener('click', function(){
+    const left = 1;
+    sliderFn(slider1,slider1Section,slider1Btns,left);
 });
